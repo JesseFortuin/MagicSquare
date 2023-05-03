941015d1570943b0ae169e97e0854d61dbf6cff8
jesse <jessefortuin850@gmail.com>
2023-05-03 13:50:23 +0200

    Feat: Created MagicSquare.BL, implemented interface

---
+++ MagicSquare.BL/IMagicSquareValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSquare.BL
+{
+    public interface IMagicSquareValidator
+    {
+        public bool IsValid(int[][] square, int gap);
+    }
+}
---
+++ MagicSquare.BL/MagicSquareValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicSquare.BL
+{
+    public class MagicSquareValidator : IMagicSquareValidator
+    {
+        public bool IsValid(int[][] square, int gap)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
